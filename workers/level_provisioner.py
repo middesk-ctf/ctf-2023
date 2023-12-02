d6d8326d4bd339911b4da30b9b7ea66e359edf27
Stewart Park <hello@stewartjpark.com>
2023-12-02 11:13:41 -0800

    complete workers

--- workers/level_provisioner.py
+++ workers/level_provisioner.py
@@ -1,11 +1,11 @@
 import random
-from typing import Literal, TypedDict, Union
+from typing import Literal, Optional, TypedDict, Union
 
 import functions_framework
 import yaml
 from cloudevents.http import CloudEvent
 from google.cloud import firestore
-from kr8s.objects import object_from_spec
+from kr8s.objects import Namespace, object_from_spec
 from mako.template import Template
 from util import get_k8s, get_pubsub_json_payload
 
@@ -15,8 +15,10 @@ db = firestore.Client()
 class ProvisionLevelPayload(TypedDict):
     action: Union[Literal["create"], Literal["destroy"]]
     player_id: str
-    level: str
-    variables: dict
+
+    # When creating,
+    level: Optional[str]
+    variables: Optional[dict]
 
 
 @functions_framework.cloud_event
@@ -25,27 +27,27 @@ def provision_level(cloud_event: CloudEvent) -> None:
     action, player_id, level, variables = (
         payload["action"],
         payload["player_id"],
-        payload["level"],
-        payload["variables"],
+        payload.get("level"),
+        payload.get("variables", {}),
     )
 
     players_collection = db.collection("players")
     player_doc = players_collection.document(player_id)
     if not player_doc.get().exists:
         raise Exception(f"unknown player: {player_id}")
-
     player = player_doc.get().to_dict()
-    namespace = f"lv-{level}-{random.randint(0, 2**32)}"
-    variables["namespace"] = namespace
-    variables["player"] = player
-    level_yaml = Template(filename=f"./levels/{level}.yaml").render(**variables)
-    k8s = get_k8s()
-    yamls = list(yaml.safe_load_all(level_yaml))
-    k8s_objects = list(object_from_spec(doc, _asyncio=False) for doc in yamls)
 
-    print("Rendered YAMLs:", yamls)
+    k8s = get_k8s()
 
     if action == "create":
+        namespace = f"lv-{level}-{random.randint(0, 2**32)}"
+        variables["namespace"] = namespace
+        variables["player"] = player
+        level_yaml = Template(filename=f"./levels/{level}.yaml").render(**variables)
+        yamls = list(yaml.safe_load_all(level_yaml))
+        k8s_objects = list(object_from_spec(doc, _asyncio=False) for doc in yamls)
+        print("Rendered YAMLs:", yamls)
+
         player_doc.update(
             {
                 "deployment": {
@@ -64,6 +66,7 @@ def provision_level(cloud_event: CloudEvent) -> None:
             {
                 "deployment": {
                     "status": "ready",
+                    "namespace": namespace,
                     "app_url": f"http://{namespace}.ctf.middesk.com/",
                 },
             }
@@ -77,12 +80,8 @@ def provision_level(cloud_event: CloudEvent) -> None:
             }
         )
 
-        for obj in k8s_objects:
-            try:
-                obj.delete()
-            except Exception as e:
-                print(e)
+        Namespace.get(player["deployment"]["namespace"]).delete()
 
-        player_doc.update({"deployment": {"status": None}})
+        player_doc.update({"deployment": {}})
     else:
         raise Exception(f"unknown action: {action}")
