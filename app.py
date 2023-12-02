56c17f08e5f162d8154a5654cea6007431af5254
Joshua Hawn <joshua@middesk.com>
2023-12-02 07:09:57 -0800

    join working

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 import logging
 import os
 
@@ -5,6 +6,7 @@ from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
 from flask import Flask, request, make_response
+from google.cloud import firestore
 
 
 load_dotenv(dotenv_path=".env.local")
@@ -12,7 +14,20 @@ load_dotenv(dotenv_path=".env.local")
 LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
 logging.basicConfig(level=LOGLEVEL)
 
+# Initialize Firestore
+db = firestore.Client()
+
 app = App()
+
+
+def dm_channel_id(user_id, client):
+    response = client.conversations_open(users=user_id)
+    return response["channel"]["id"]
+
+
+def dm_user(user_id, client, message):
+    channel_id = dm_channel_id(user_id, client)
+    client.chat_postMessage(channel=channel_id, text=message)
 
 
 # Listens to incoming messages that contain "ctf-bot"
@@ -25,11 +40,10 @@ def hello_from_ctf_bot(message, say):
 def ctf_usage():
     commands = [
         "join",
-        "standings",
         "standings [LEVEL]",
         "start",
         "restart",
-        "capture [FLAG]",
+        "capture FLAG"
     ]
     usage = "Valid commands are:"
     for command in commands:
@@ -37,36 +51,58 @@ def ctf_usage():
     return usage
 
 
-def unrecognized_command():
-    return f"Sorry, I don't recognize your command.\n{ctf_usage()}"
+def dm_unrecognized_command(user_id, client):
+    dm_user(user_id, client, f"Sorry, I don't recognize your command.\n{ctf_usage()}")
 
 
 @app.command("/ctf")
-def ctf_command(ack, command, say):
-    ack()  # Acknowledge the command request.
+def ctf_command(ack, command, client, say):
+    ack() # Acknowledge the command request.
 
-    print(f"\n{command}\n")
+    print(f'Got Command:\n{command}\n')
 
-    text = command["text"]
-    if text == "join":
-        user_id = command["user_id"]
-        # Add user_id to firestore collection of players.
-        say(
-            f"Hi, <@{user_id}>. You've been added to the CTF competition! (not really)\nI'll notify you when the 1st level is ready!"
-        )
-    elif text == "standings":
-        say(f"The CTF competition has not yet started. Please check back later!")
-    elif text.startswith("standings "):
-        say(f"The CTF competition has not yet started. Please check back later!")
-    elif text.startswith("capture "):
-        say(f"The CTF competition has not yet started. Please check back later!")
-    else:
-        say(unrecognized_command())
+    user_id = command['user_id']
+
+    args = command['text'].split(' ')
+    if not args:
+        return dm_unrecognized_command(user_id, client)
+    
+    if args[0] == 'join':
+        return handle_join(args[1:], user_id, client)
+
+    return dm_unrecognized_command(user_id, client)
 
 
-# TODO
-def handle_join(slack_user_id):
-    pass
+def handle_join(args, user_id, client):
+    if args:
+        # There should be no arguments.
+        return dm_unrecognized_command(user_id, client)
+    
+    # Player IDs are Slack User IDs.
+    player_id = user_id
+
+    # Firestore: Check if the user already exists
+    players_collection = db.collection('players')
+    player_doc = players_collection.document(player_id).get()
+
+    if player_doc.exists:
+        return dm_user(user_id, client, f"Welcome back <@{user_id}>! You are already registered.")
+
+    # Fetch user info from Slack
+    user_info = client.users_info(user=user_id)
+    user_email = user_info['user']['profile']['email']
+
+    # Add new player to Firestore
+    players_collection.document(user_id).set({
+        "id": user_id,
+        "email": user_email,
+        "joined_at": datetime.now(tz=timezone.utc).isoformat(),
+        "current_level": 0,
+        "deployment": {},
+        "secret_flag": ""
+    })
+    
+    dm_user(user_id, client, f"Welcome <@{user_id}>! You have been successfully registered.")
 
 
 def handle_standings(level=None):
